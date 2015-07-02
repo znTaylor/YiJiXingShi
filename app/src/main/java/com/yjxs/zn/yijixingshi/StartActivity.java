bfafac1b9f87d0c988025b4be7c32127e7fe84b2
lafengde9 <lafengde9@126.com>
2015-07-02 16:07:41 +0800

    add start activity

---
+++ app/src/main/java/com/yjxs/zn/yijixingshi/StartActivity.java
@@ -0,0 +1,32 @@
+package com.yjxs.zn.yijixingshi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+
+public class StartActivity extends Activity {
+
+
+    private static final int START_DELAY_SECONDS = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.start);
+
+        //ÔÝÍ£3Ãëºó£¬Ìø×ªµ½MainActivity
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                StartActivity.this.startActivity(intent);
+                StartActivity.this.finish();
+            }
+        }, START_DELAY_SECONDS);
+
+    }
+
+
+}
