4ccc42a9ef6cfd6f1768572903c3febe916a6021
lafengde9 <lafengde9@126.com>
2015-07-08 15:25:20 +0800

    增加工具类HttpUtil，修改用户名检查，使用HttpUtil

--- app/src/main/java/com/yjxs/zn/yijixingshi/RegisterActivity.java
+++ app/src/main/java/com/yjxs/zn/yijixingshi/RegisterActivity.java
@@ -6,17 +6,16 @@ import android.os.Message;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yjxs.zn.yijixingshi.util.CommonUtil;
+import com.yjxs.zn.yijixingshi.util.HttpUtil;
 import com.yjxs.zn.yijixingshi.view.LoadingDialog;
 
+import org.apache.http.client.methods.HttpPost;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -32,7 +31,7 @@ public class RegisterActivity extends Activity {
 
     //form
     private EditText userName,password,email,passwordConfirm;
-    CharSequence strUserName;
+    CharSequence strUserName,strPassword,strPasswordConfirm,strEmail;
     LoadingDialog dialog;
 
 
@@ -40,41 +39,40 @@ public class RegisterActivity extends Activity {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            Bundle data = msg.getData();
-            String val = data.getString("result");
-            Log.i("RegisterActivity", "请求结果为-->" + val);
-            dialog.dismiss();
+            if (msg.what == 100) {
+                Bundle data = msg.getData();
+                String val = data.getString("result");
+                Log.i("RegisterActivity", "请求结果为-->" + val);
+                dialog.dismiss();
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            // UI界面的更新等相关操作
-            try {
-                JSONObject jsonResult = new JSONObject(val);
-
-                if (jsonResult.getString("result").equals("0")) { //用户名可用
-                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"用户名可用",true);
-                    toast.show();
-
-                }
-                else if (jsonResult.getString("result").equals("1")){
-                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"用户名已存在",false);
-                    toast.show();
-                }
-                else {
-                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"用户名检查失败",false);
-                    toast.show();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
 
-            } catch (JSONException e){
-                e.printStackTrace();
-            }
+                // UI界面的更新等相关操作
+                try {
+                    JSONObject jsonResult = new JSONObject(val);
+
+                    if (jsonResult.getString("result").equals("0")) { //用户名可用
+                        Toast toast = CommonUtil.showToast(RegisterActivity.this, "用户名可用", true);
+                        toast.show();
+
+                    } else if (jsonResult.getString("result").equals("1")) {
+                        Toast toast = CommonUtil.showToast(RegisterActivity.this, "用户名已存在", false);
+                        toast.show();
+                    } else {
+                        Toast toast = CommonUtil.showToast(RegisterActivity.this, "用户名检查失败", false);
+                        toast.show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
 
-
+            } //end of msg.what == 100
 
         }
     };
@@ -86,6 +84,7 @@ public class RegisterActivity extends Activity {
 
         initControls();
 
+        //检查用户名
         checkUserName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -108,10 +107,55 @@ public class RegisterActivity extends Activity {
             }
         });
 
-
+        // 注册
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                strUserName = userName.getText();
+                if (TextUtils.isEmpty(strUserName))
+                {
+                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"用户名不能为空",false);
+                    toast.show();
+                    return;
+                }
+                strEmail = email.getText();
+                if (TextUtils.isEmpty(strEmail)){
+                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"邮箱不能为空",false);
+                    toast.show();
+                    return;
+                }
+                strPassword = password.getText();
+                if (TextUtils.isEmpty(strPassword)){
+                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"密码不能为空",false);
+                    toast.show();
+                    return;
+                }
+                strPasswordConfirm = passwordConfirm.getText();
+                if (TextUtils.isEmpty(strPasswordConfirm)){
+                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"密码输入不一致",false);
+                    toast.show();
+                    return;
+                }
+
+                //检查邮箱
+                if (!CommonUtil.checkEmail(strEmail.toString())){
+                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"邮箱格式不正确",false);
+                    toast.show();
+                    return;
+                }
+                //检查密码的一致性
+                if (!strPassword.equals(strPasswordConfirm)){
+                    Toast toast = CommonUtil.showToast(RegisterActivity.this,"密码输入不一致",false);
+                    toast.show();
+                    return;
+                }
+
+                dialog = new LoadingDialog(RegisterActivity.this,
+                        R.style.loading_dialog,getString(R.string.register_save_user));
+                dialog.show();
+                //POST方式注册
+                saveRegisterUser();
+
 
             }
         });
@@ -133,32 +177,18 @@ public class RegisterActivity extends Activity {
         Runnable networkTask = new Runnable() {
             @Override
             public void run() {
-                String strUrl = "http://120.27.46.194/ZnConsole/business/check_username.php?username=" +
-                        strUserName;
-                URL url;
-                try {
-                    url = new URL(strUrl);
-                    HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-                    InputStreamReader in = new InputStreamReader(urlConn.getInputStream());
-                    BufferedReader bufferReader = new BufferedReader(in);
-                    String result = "";
-                    String readLine;
-                    while ((readLine = bufferReader.readLine()) != null) {
-                        result += readLine;
-                    }
+                String strUrl = "http://120.27.46.194/ZnConsole/business/check_username.php";
+                String params = "username=" + strUserName;
+                String response = HttpUtil.sendHttpGet(strUrl, params);
 
-                    Message msg = new Message();
-                    Bundle data = new Bundle();
-                    data.putString("result", result);
-                    msg.setData(data);
-                    handler.sendMessage(msg);
+                Message msg = new Message();
+                msg.what = 100;
+                Bundle data = new Bundle();
+                data.putString("result", response);
+                msg.setData(data);
+                handler.sendMessage(msg);
 
-                    in.close();
-                    urlConn.disconnect();
 
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
             }
         };
 
@@ -166,5 +196,17 @@ public class RegisterActivity extends Activity {
     }
 
 
+    private void saveRegisterUser(){
+        Runnable networkTask = new Runnable() {
+            @Override
+            public void run() {
+                String strUrl = "http://120.27.46.194/ZnConsole/business/register_user.php";
+
+
+            }
+        };
+
+        new Thread(networkTask).start();
+    }
 
 }
--- app/src/main/java/com/yjxs/zn/yijixingshi/util/CommonUtil.java
+++ app/src/main/java/com/yjxs/zn/yijixingshi/util/CommonUtil.java
@@ -9,10 +9,15 @@ import android.widget.Toast;
 
 import com.yjxs.zn.yijixingshi.R;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class CommonUtil {
 
     public static Toast showToast(Context context,String content,boolean status){
-        Toast toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0, 0);
         View v = View.inflate(context, R.layout.layout_custom_toast, null);
         ImageView image = (ImageView) v.findViewById(R.id.toast_image);
@@ -26,6 +31,18 @@ public class CommonUtil {
         toast.setView(v);
 
         return toast;
+    }
+
+    public static boolean checkEmail(String email)
+    {
+        boolean tag = true;
+        final String pattern1 = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+        final Pattern pattern = Pattern.compile(pattern1);
+        final Matcher mat = pattern.matcher(email);
+        if (!mat.find()) {
+            tag = false;
+        }
+        return tag;
     }
 
 }
