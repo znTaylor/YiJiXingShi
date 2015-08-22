d9b1a1369b75c64d19e7ba91972c5c8568bb96a3
znTaylor <lafengde9@126.com>
2015-08-22 15:31:58 +0800

    月计划制定初始化界面显示年计划细节

--- app/src/main/java/com/yjxs/zn/yijixingshi/MonthPlanInitActivity.java
+++ app/src/main/java/com/yjxs/zn/yijixingshi/MonthPlanInitActivity.java
@@ -1,16 +1,129 @@
 package com.yjxs.zn.yijixingshi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yjxs.zn.yijixingshi.util.CommonUtil;
+import com.yjxs.zn.yijixingshi.util.HttpUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MonthPlanInitActivity extends BaseActivity {
+
+    private TextView currYearPlan,monthPlanTitle,monthPlanContent,beginAndEndMonth,monthToBePlaned;
+    private Button makeMonthPlan;
+    private ImageView monthPlanBack;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (msg.what == 120) {
+                Bundle data = msg.getData();
+                String val = data.getString("result");
+                Log.i("MonthPlanInitActivity", "请求结果为-->" + val);
+
+                // UI界面的更新等相关操作
+                try {
+                    JSONObject jsonResult = new JSONObject(val);
+                    String yearplanid = jsonResult.getString("yearplanid");
+                    String yearplantitle = jsonResult.getString("yearplantitle");
+                    String yearplancontent = jsonResult.getString("yearplancontent");
+                    String beginmonth = jsonResult.getString("beginmonth");
+
+                    currYearPlan.setText(yearplanid);
+                    monthPlanTitle.setText(yearplantitle);
+                    monthPlanContent.setText(yearplancontent);
+                    beginAndEndMonth.setText(beginmonth+"-"+"12");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_plan_init);
+
+        initControls();
     }
 
 
+    /**
+     * 初始化控件
+     * */
+    private void initControls(){
+        currYearPlan = (TextView) this.findViewById(R.id.curr_year_plan);
+        monthPlanTitle = (TextView) this.findViewById(R.id.month_plan_title);
+        monthPlanContent = (TextView) this.findViewById(R.id.month_plan_content);
+        beginAndEndMonth = (TextView) this.findViewById(R.id.month_begin_end_date);
+        monthToBePlaned = (TextView) this.findViewById(R.id.choose_month);
+
+        //初始化数据
+        getYearPlanDetail();
+
+        makeMonthPlan = (Button) this.findViewById(R.id.button_make_month_plan);
+        makeMonthPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        monthPlanBack = (ImageView) this.findViewById(R.id.month_plan_back);
+        monthPlanBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void getYearPlanDetail(){
+        final String userName = CommonUtil.ReadSharedPreferences(MonthPlanInitActivity.this,
+                "login_user");
+        Runnable networkTask = new Runnable() {
+            @Override
+            public void run() {
+                String strUrl = "http://120.27.46.194/ZnConsole/business/get_year_plan.php";
+                String params = "username=" + userName;
+                String response = HttpUtil.sendHttpPost(strUrl, params);
+
+                Message msg = new Message();
+                msg.what = 120;
+                Bundle data = new Bundle();
+                data.putString("result", response);
+                msg.setData(data);
+                handler.sendMessage(msg);
+
+            }
+        };
+
+        new Thread(networkTask).start();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(MonthPlanInitActivity.this,MainActivity.class);
+        startActivity(intent);
+        MonthPlanInitActivity.this.finish();
+    }
 
 }
