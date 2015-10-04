8c06d718a5ed453a9ea5b2b042c32d046136e148
znTaylor <lafengde9@126.com>
2015-10-04 17:02:40 +0800

    refactor--年计划制定界面修改：点击按钮，添加计划并缓存

--- app/src/main/java/com/yjxs/zn/yijixingshi/fragment/MakeYearPlanFragment.java
+++ app/src/main/java/com/yjxs/zn/yijixingshi/fragment/MakeYearPlanFragment.java
@@ -1,14 +1,20 @@
 package com.yjxs.zn.yijixingshi.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yjxs.zn.yijixingshi.R;
@@ -28,6 +34,21 @@ public class MakeYearPlanFragment extends Fragment {
     int[] buttonIds = {R.id.month1,R.id.month2,R.id.month3,R.id.month4,R.id.month5,R.id.month6,
         R.id.month7,R.id.month8,R.id.month9,R.id.month10,R.id.month11,R.id.month12};
     Button[] monthButton;
+
+    //其它变量
+    /**
+     * 保存年计划的详情，即每个月的计划内容
+     * */
+    private String[] yearPlanMonthDetail = {"","","","","","","","","","","",""};
+
+    /**
+     * 正在编辑的月份
+     * */
+    private int editingMonth;
+    /**
+     * 已经编辑过的月份
+     * */
+    private int editedMonth;
 
 
     public MakeYearPlanFragment() {
@@ -75,6 +96,27 @@ public class MakeYearPlanFragment extends Fragment {
 
             }
         });
+
+        inputShow.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus){
+                    Log.d(TAG, "READY TO INPUT");
+                }
+                else {
+                    Log.d(TAG, "INPUT COMPLETED");
+                    yearPlanMonthDetail[editedMonth] = inputShow.getText().toString();
+                    if (!TextUtils.isEmpty(inputShow.getText().toString())){
+                        inputShow.setText(null);
+                    }
+                    else{
+                        monthButton[editedMonth].setBackgroundResource(R.drawable.round_button_selector);
+                    }
+                }
+            }
+        });
+
+
         monthButton = new Button[buttonIds.length];
         for (int i=0; i<buttonIds.length; i++){
             final int index = i;
@@ -82,7 +124,14 @@ public class MakeYearPlanFragment extends Fragment {
             monthButton[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    if (inputShow.isFocused()) {
+                        inputShow.clearFocus();//输入框失去焦点
+                    }
+                    editingMonth = index;
+                    inputShow.setText(getSingleMonthPlan(editingMonth));
+                    inputShow.requestFocus();//输入框获取焦点(layout文件中设置focusable=true)
+                    editedMonth = editingMonth;
+                    monthButton[index].setBackgroundResource(R.drawable.round_button_selector2);
                 }
             });
         }
@@ -132,5 +181,11 @@ public class MakeYearPlanFragment extends Fragment {
         }
     }
 
+    /**
+     * 获取缓存的指定月的计划
+     * */
+    private String getSingleMonthPlan(int month){
+        return yearPlanMonthDetail[month];
+    }
 
 }
